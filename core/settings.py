c19a7147fd7a050bf792e8407836f4b0f7d077a7
Cesar Mora <cmc@ingtk3dev.ingeniotek.com.co>
2025-12-30 15:15:57 -0500

    Add src path for imports. Create theme app for Tailwind.

--- core/settings.py
+++ core/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import sys
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Añadir 'src' al path de Python para hacer importaciones directamente
+sys.path.append(os.path.join(BASE_DIR, 'src'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
